0764802757f8dea5c7c8183623dcbbb4e5a46385
Katie <katehodges@me.com>
2021-02-02 13:17:43 -0500

    sending messages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,21 @@
 (() => {
     console.log('fired');
+// 
+    const socket = io();
+
+    const vm = new Vue({
+        data: {
+            messages: [],
+            nickname: "",
+            username: "",
+        },
+
+        created: function() {
+            console.log('its alive');
+        },
+
+        methods: {
+
+        }
+    }).$mount("#app");
 }) ();
