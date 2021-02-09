a1ce3868f1072d40ff42b7794cb75360041d063a
Katie <katehodges@me.com>
2021-02-09 11:03:23 -0500

    adding vue functions

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,30 @@
+import ChatMessage from "./components/TheMessageComponent.js"
+
 (() => {
     console.log('fired');
-// 
+// refers to socket.io library and makes a connection to app.js
     const socket = io();
+
+//messenger service event handling -> incoming from the manager
+    function setUserId({sID, message}) {
+        //incoming connected event with data
+        // debugger;
+
+        vm.socketID = sID;
+    }
+
+    function appendMessage(message) {
+        vm.messages.push(message);
+    }
 
     const vm = new Vue({
         data: {
+            //singular message = send, plural message(s) = receive
             messages: [],
             nickname: "",
             username: "",
+            socketID: "",
+            message: ""
         },
 
         created: function() {
@@ -15,7 +32,18 @@
         },
 
         methods: {
+            dispatchMessage() {
+                // debugger;
+                socket.emit('chatmessage', {content: this.message, name: this.nickname || "Anonymous"})
+            }
+        },
 
+        components: {
+            newmessage: ChatMessage
         }
+
     }).$mount("#app");
+
+    socket.addEventListener('connected', setUserId);
+    socket.addEventListener('message', appendMessage);
 }) ();
